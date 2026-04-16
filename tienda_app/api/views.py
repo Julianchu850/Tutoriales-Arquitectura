a705b32be5f26618737144db1c03c4df8f530a5e
Julianchu850 <jlaraa@eafit.edu.co>
2026-04-16 11:06:45 -0500

    Fix bug no pasa datos

--- tienda_app/api/views.py
+++ tienda_app/api/views.py
@@ -43,5 +43,5 @@ class CompraAPIView(APIView):
 
         except ValueError as e:
             return Response({'error': str(e)}, status=status.HTTP_409_CONFLICT)
-        except Exception:
-            return Response({'error': 'Error interno'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
